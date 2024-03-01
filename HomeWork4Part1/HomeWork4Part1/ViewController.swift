37398d795cfba1543f8d56360681c0ff77da59f9
Oleksandr-Matsenko <olexandr282@gmail.com>
2024-03-01 15:08:20 +0200

    Add UIButton
    add action for Button

--- HomeWork4Part1/HomeWork4Part1/ViewController.swift
+++ HomeWork4Part1/HomeWork4Part1/ViewController.swift
@@ -9,16 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var wellcomeLabel: UILabel!
     
-
+    @IBOutlet weak var someButton: UIButton!
+    private var isOn = false
     override func viewDidLoad() {
         super.viewDidLoad()
         wellcomeLabel.text = "We are glad to see you here!"
       
     }
 
-
+    
+    @IBAction func tupButtonAction(_ sender: UIButton) {
+        isOn.toggle()
+        if isOn {
+            wellcomeLabel.text = "Wellcome to programming World"
+            isOn = true
+        }
+        else {
+            wellcomeLabel.text = "We are glad to see you here!"
+            isOn = false
+        }
+    }
+    
 }
 
